2b1ed7272511a1dbf754263ae75ae429ca243cba
Samuel Oosterhuis <coder@sammyo.com>
2015-01-03 14:20:49 -0500

    dates entered

---
+++ sun.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from datetime import datetime
+
+winter = datetime.strptime('Dec 21 2014  7:51AM', '%b %d %Y %I:%M%p')
+summer = datetime.strptime('Jun 21 2005  5:40AM', '%b %d %Y %I:%M%p')
+
+
+
+# vim: set noexpandtab ts=4 sw=4 foldmethod=marker:
